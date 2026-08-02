67f9170f16f353ed669463cf26805ef28d69504e
bartkovasv-ship-it <234063346+bartkovasv-ship-it@users.noreply.github.com>
2026-08-02 20:09:23 +0300

    start

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import telebot
 
-print('hello')
+bot=telebot.TeleBot('8953354779:AAEVWRhsADFt5NtNKJl0YUWAQ4275u0pva8')
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, 'Привет!')
 
 
