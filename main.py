999fe2944c60e2f1b50d2eb73843f3c135ad0c9b
bartkovasv-ship-it <234063346+bartkovasv-ship-it@users.noreply.github.com>
2026-08-02 20:21:22 +0300

    Обращение к пользователю

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ bot=telebot.TeleBot('8953354779:AAEVWRhsADFt5NtNKJl0YUWAQ4275u0pva8')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, 'Привет!')
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}!')
 
 
+
+bot.polling(none_stop=True)
