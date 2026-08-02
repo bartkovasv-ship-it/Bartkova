2eeea575a0771d66b6e08dbe429568d18d9ba280
bartkovasv-ship-it <234063346+bartkovasv-ship-it@users.noreply.github.com>
2026-08-02 20:45:34 +0300

    первые кнопки

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import telebot
+from telebot import types
 
 bot=telebot.TeleBot('8953354779:AAEVWRhsADFt5NtNKJl0YUWAQ4275u0pva8')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Создать новую привычку',callback_data='make'))
+    markup.add(types.InlineKeyboardButton('Удалить привычку', callback_data='delete'))
+    markup.add(types.InlineKeyboardButton('Отметить выполнение', callback_data='mark'))
     bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}!')
-
 
 
 bot.polling(none_stop=True)
