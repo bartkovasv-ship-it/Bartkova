681a95586f8d34f2b68f63d8dec2622a494267d6
bartkovasv-ship-it <234063346+bartkovasv-ship-it@users.noreply.github.com>
2026-08-03 14:20:21 +0300

    Исправила ошибки работы с БД и кнопки

--- main.py
+++ main.py
@@ -12,24 +12,37 @@ def send_welcome(message):
 
     conn=sqlite3.connect('data.sql')
     cur=conn.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS habits (id int auto_increment primary key, user_id integer, name varchar(255))')
-    cur.execute('CREATE TABLE IF NOT EXISTS progress(id INTEGER PRIMARY KEY AUTOINCREMENT, habit_id INTEGER, date TEXT)')
+
+    cur.execute('CREATE TABLE IF NOT EXISTS habits (id int auto_increment primary key, user_id INTEGER, name TEXT)')
+    cur.execute('CREATE TABLE IF NOT EXISTS progress(id int auto_increment primary key, habit_id INTEGER, date TEXT)')
+    #cur.execute('CREATE TABLE IF NOT EXISTS habits (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, name TEXT)')
+    #cur.execute('CREATE TABLE IF NOT EXISTS progress(id INTEGER PRIMARY KEY AUTOINCREMENT, habit_id INTEGER, date TEXT)')
+
     conn.commit()
+
+    cur.execute("SELECT * FROM habits WHERE user_id=?",(message.from_user.id,))
+    habits=cur.fetchall()
+
     cur.close()
     conn.close()
 
 
     markup = types.InlineKeyboardMarkup()
+
     markup.add(types.InlineKeyboardButton('Создать новую привычку',callback_data='add'))
     markup.add(types.InlineKeyboardButton('Удалить привычку', callback_data='delete'))
     markup.add(types.InlineKeyboardButton('Отметить выполнение', callback_data='mark'))
 
+    make = types.InlineKeyboardMarkup()
+    make.add(types.InlineKeyboardButton('Создать новую привычку',callback_data='add'))
+
     bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}!')
     if not habits:
-        bot.send_message(message.chat.id, 'У вас пока нет привычек.')
-        bot.send_message(message.chat.id,'Добавим новую?')
-        markup.add(types.InlineKeyboardButton('Создать новую привычку', callback_data='add'))
-
+        bot.send_message(message.chat.id, 'У вас пока нет привычек./n Добавим новую?',reply_markup=make)
+        #bot.send_message(message.chat.id,'Добавим новую?',reply_markup=make)
+        #markup.add(types.InlineKeyboardButton('Создать новую привычку', callback_data='add'))
+    else:
+        bot.send_message(message.chat.id,'Ваше меню:', reply_markup=markup)
 #def add_habit(message):
     
 bot.polling(none_stop=True)
