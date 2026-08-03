ce88e09185a1f503ad814f0980435aba30847ccc
bartkovasv-ship-it <234063346+bartkovasv-ship-it@users.noreply.github.com>
2026-08-03 15:07:55 +0300

    Вывод всех привычек с возможностью тут же их выполнить

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fileinput import close
 import telebot
 from telebot import types
 import sqlite3
+from datatime import datetime
 
 bot=telebot.TeleBot('8953354779:AAEVWRhsADFt5NtNKJl0YUWAQ4275u0pva8')
 user_action={}
@@ -32,6 +33,7 @@ def send_welcome(message):
     markup.add(types.InlineKeyboardButton('Создать новую привычку',callback_data='add'))
     markup.add(types.InlineKeyboardButton('Удалить привычку', callback_data='delete'))
     markup.add(types.InlineKeyboardButton('Отметить выполнение', callback_data='mark'))
+    markup.add(types.InlineKeyboardButton('Мои привычки',callback_data='list'))
 
     make = types.InlineKeyboardMarkup()
     make.add(types.InlineKeyboardButton('Создать новую привычку',callback_data='add'))
@@ -55,7 +57,26 @@ def button(call):
         bot.send_message(call.message.chat.id,"Удаление")
     elif call.data=='mark':
         bot.send_message(call.message.chat.id,"Отмечание")
+    elif call.data=='list':
 
+        conn = sqlite3.connect('data.sql')
+        cur = conn.cursor()
+
+        cur.execute("SELECT id, name FROM habits WHERE user_id=?", (call.from_user.id,))
+
+        habits=cur.fetchall()
+
+        conn.close()
+
+        if not habits:
+            bot.send_message(message.chat.id, 'У тебя пока нет привычек.\nДобавим новую?', reply_markup=make)
+        else:
+            for habit in habits:
+
+                markup = types.InlineKeyboardMarkup()
+                markup.add(types.InlineKeyboardButton("Выполнено", callback_data=f"done_{habit[0]}"))
+
+                bot.send_message(call.message.chat.id,f"{habit[1]}",reply_markup=markup)
 @bot.message_handler(func=lambda message: user_action.get(message.chat.id)=="add_habit")
 def save_habits(message):
     conn=sqlite3.connect('data.sql')
@@ -71,5 +92,6 @@ def save_habits(message):
     user_action.pop(message.chat.id)
 
     bot.send_message(message.chat.id,f"Привычка '{message.text}' добавлена!")
+
 
 bot.polling(none_stop=True)
