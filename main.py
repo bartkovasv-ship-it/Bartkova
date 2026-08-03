b86b747500d0fac9d41cac0586275608c9c0ea3b
bartkovasv-ship-it <234063346+bartkovasv-ship-it@users.noreply.github.com>
2026-08-03 11:55:01 +0300

    таблица

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+from fileinput import close
+
 import telebot
 from telebot import types
+import sqlite3
 
 bot=telebot.TeleBot('8953354779:AAEVWRhsADFt5NtNKJl0YUWAQ4275u0pva8')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+
+
+    conn=sqlite3.connect('data.sql')
+    cur=conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXIST habits (id int auto_increment primary key, user_id integer, name varchar(255))')
+    cur.execute('CREATE TABLE IF NOT EXISTS progress(id INTEGER PRIMARY KEY AUTOINCREMENT, habit_id INTEGER, date TEXT)')
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton('Создать новую привычку',callback_data='make'))
     markup.add(types.InlineKeyboardButton('Удалить привычку', callback_data='delete'))
